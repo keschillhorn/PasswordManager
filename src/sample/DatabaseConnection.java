ebc1b84042526243e4b5300e02dfc027929fb195
keschillhorn <74057366+keschillhorn@users.noreply.github.com>
2021-04-16 17:00:18 -0400

    Update DatabaseConnection.java

--- src/sample/DatabaseConnection.java
+++ src/sample/DatabaseConnection.java
@@ -9,7 +9,7 @@ public class DatabaseConnection {
     public Connection getConnection() {
         String databaseName = "login";
         String databaseUser = "root";
-        String databasePassword = "Cps50012957";
+        String databasePassword = "password";
         String url = "jdbc:mysql://localhost/" + databaseName;
 
         try {
